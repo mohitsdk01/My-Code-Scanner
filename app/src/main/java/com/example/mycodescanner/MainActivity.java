bef7dc5236c9a5ad55502e7489207c022dc1d90f
Mohitdk <mohitdk2@gmail.com>
2023-01-09 23:23:41 +0530

    URL redirect to browser implementation

--- app/src/main/java/com/example/mycodescanner/MainActivity.java
+++ app/src/main/java/com/example/mycodescanner/MainActivity.java
@@ -6,10 +6,14 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
+import android.util.Patterns;
 import android.view.View;
 import android.webkit.URLUtil;
 import android.widget.TextView;
@@ -20,6 +24,7 @@ import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
 import com.budiyev.android.codescanner.ScanMode;
 import com.google.zxing.Result;
+import com.google.zxing.client.result.EmailAddressParsedResult;
 
 public class MainActivity extends AppCompatActivity {
     private CodeScanner mCodeScanner;
@@ -48,13 +53,34 @@ public class MainActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            //Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+//                            Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
                             String data = result.getText();
-//                            if(URLUtil.isValidUrl(data)){
-//                                outputClick(View );
-//                            }else {
-                                textView.setText(data);
+                            if(Patterns.WEB_URL.matcher(data).matches()){
+                                Intent browserIntent =new Intent(Intent.ACTION_VIEW, Uri.parse(data));
+                                startActivity(browserIntent);
+                            }
+
+                            else if(Patterns.EMAIL_ADDRESS.matcher(data).matches()){
+
+                                textView.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        //Uri emailAddress = Uri.parse(data);
+                                        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                                        emailIntent.setData(Uri.parse("mailto:"));
+                                        startActivity(emailIntent);
+                                    }
+                                });
+                            }
+
+//                            else if(Patterns.PHONE.matcher(data).matches()){
+//                                Uri phone = Uri.parse(data);
+//                                Intent phone = new Intent(Intent.EXTRA_PHONE_NUMBER)
 //                            }
+
+                            else {
+                                textView.setText(data);
+                            }
                         }
                     });
                 }
@@ -67,30 +93,6 @@ public class MainActivity extends AppCompatActivity {
             });
         }
     }
-
-//    private void runScanner() {
-//            mCodeScanner = new CodeScanner(this, scannerView);
-//            mCodeScanner.setDecodeCallback(new DecodeCallback() {
-//                @Override
-//                public void onDecoded(@NonNull final Result result) {
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            //Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
-//                            String data = result.getText();
-//                            textView.setText(data);
-//                        }
-//                    });
-//                }
-//            });
-//            scannerView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    mCodeScanner.startPreview();
-//                }
-//            });
-//        }
-//    }
 
     public static boolean hasPermissions(Context context, String... permissions){
         if(context != null && permissions != null){
@@ -115,6 +117,4 @@ public class MainActivity extends AppCompatActivity {
         mCodeScanner.releaseResources();
         super.onPause();
     }
-
-
 }
